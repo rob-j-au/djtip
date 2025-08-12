82bd97185fc68e7cb353db1ef7f260221d226b5e
rob-j-au <rob@jennings.au>
2025-08-12 22:15:47 +1000

    use Flowbite Datepicker and dropdowns for Time forms

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,7 @@ class EventsController < ApplicationController
   # POST /events or /events.json
   def create
     @event = Event.new(event_params)
+    combine_date_and_time(@event)
 
     respond_to do |format|
       if @event.save
@@ -36,8 +37,11 @@ class EventsController < ApplicationController
 
   # PATCH/PUT /events/1 or /events/1.json
   def update
+    @event.assign_attributes(event_params)
+    combine_date_and_time(@event)
+    
     respond_to do |format|
-      if @event.update(event_params)
+      if @event.save
         format.html { redirect_to @event, notice: "Event was successfully updated." }
         format.json { render :show, status: :ok, location: @event }
       else
@@ -65,6 +69,79 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :description, :date, :location)
+      params.require(:event).permit(:title, :description, :date, :time, :hour, :minute, :ampm, :location)
+    end
+
+    # Combine separate date and time fields into a single datetime
+    def combine_date_and_time(event)
+      if params[:event][:date].present?
+        begin
+          date_str = params[:event][:date]
+          
+          # Parse the date from the datepicker format (mm/dd/yyyy)
+          date_part = Date.strptime(date_str, "%m/%d/%Y") rescue Date.parse(date_str)
+          
+          # Handle dropdown time fields (hour, minute, ampm)
+          if params[:event][:hour].present? && params[:event][:minute].present? && params[:event][:ampm].present?
+            hour = params[:event][:hour].to_i
+            minute = params[:event][:minute].to_i
+            ampm = params[:event][:ampm]
+            
+            # Convert 12-hour format to 24-hour format
+            if ampm == 'AM' && hour == 12
+              hour = 0
+            elsif ampm == 'PM' && hour != 12
+              hour += 12
+            end
+            
+            # Combine date and time
+            combined_datetime = DateTime.new(
+              date_part.year,
+              date_part.month,
+              date_part.day,
+              hour,
+              minute,
+              0
+            )
+            
+            event.date = combined_datetime
+          elsif params[:event][:time].present?
+            # Fallback to single time field if present
+            time_str = params[:event][:time]
+            time_part = Time.parse(time_str)
+            
+            combined_datetime = DateTime.new(
+              date_part.year,
+              date_part.month,
+              date_part.day,
+              time_part.hour,
+              time_part.min,
+              time_part.sec
+            )
+            
+            event.date = combined_datetime
+          else
+            # If only date is provided, set time to noon
+            combined_datetime = DateTime.new(
+              date_part.year,
+              date_part.month,
+              date_part.day,
+              12,
+              0,
+              0
+            )
+            
+            event.date = combined_datetime
+          end
+        rescue => e
+          Rails.logger.error "Error combining date and time: #{e.message}"
+          # If parsing fails, try to use the original date field
+          begin
+            event.date = DateTime.parse(params[:event][:date])
+          rescue => parse_error
+            Rails.logger.error "Error parsing date: #{parse_error.message}"
+          end
+        end
+      end
     end
 end
