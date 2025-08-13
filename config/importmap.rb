49f44f9c562d642bc48dbec7d8f6d1785a7387f6
rob-j-au <rob@jennings.au>
2025-08-13 22:40:41 +1000

    rails admin css fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,11 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "datepicker", to: "datepicker.js"
 
-# RailsAdmin dependencies
-pin "rails_admin", to: "rails_admin.js"
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"
-pin "jquery-ui", to: "https://cdn.jsdelivr.net/npm/jquery-ui@1.13.3/dist/jquery-ui.min.js"
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
-pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/popper.js"
-pin "flatpickr", to: "https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js"
-pin "@fortawesome/fontawesome-free", to: "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/js/all.min.js"
+
