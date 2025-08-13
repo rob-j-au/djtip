0d014d4df1baec5e04333ed95a41d06afed5685d
rob-j-au <rob@jennings.au>
2025-08-13 23:22:34 +1000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  root "home#index"
+  # Root route already defined above as root 'events#index'
 end
