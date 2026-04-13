71c33aafd779031442edb4a7435756770feb1978
rob-j-au <rob@jennings.au>
2026-04-14 09:52:20 +1000

    Fix TraceIdLogFormatter inheritance error
    Error: TypeError: superclass must be an instance of Class (given an instance of Module)
    Cause: ActiveSupport::TaggedLogging::Formatter is a Module, not a Class
    
    Fix: Use proc wrapper instead of class inheritance to add trace IDs
    - Wraps existing Rails logger formatter
    - Preserves tagged logging functionality
    - Adds trace ID prefix to all log messages
    - Compatible with Rails production logger setup

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,14 +1,12 @@
 # Custom logger formatter that includes OpenTelemetry trace IDs
 # This enables log-trace correlation in Grafana
 
-class TraceIdLogFormatter < ActiveSupport::TaggedLogging::Formatter
+class TraceIdLogFormatter < ActiveSupport::Logger::SimpleFormatter
   def call(severity, timestamp, progname, msg)
     trace_id = current_trace_id
     
     if trace_id
-      # Add trace ID as a tag
-      tags_text = tags.any? ? "#{tags_text} " : ""
-      "[#{trace_id}] #{tags_text}#{msg}\n"
+      "[#{trace_id}] #{super}"
     else
       super
     end
@@ -31,11 +29,24 @@ class TraceIdLogFormatter < ActiveSupport::TaggedLogging::Formatter
   end
 end
 
-# Apply custom formatter to Rails logger with tagged logging support
+# Apply custom formatter to Rails logger
+# Note: We keep the existing tagged logging functionality
 Rails.application.config.after_initialize do
-  # Use TaggedLogging wrapper with our custom formatter
-  logger = ActiveSupport::Logger.new($stdout)
-  logger.formatter = TraceIdLogFormatter.new
-  Rails.logger = ActiveSupport::TaggedLogging.new(logger)
+  if Rails.logger.formatter
+    # Wrap existing formatter to add trace IDs
+    original_formatter = Rails.logger.formatter
+    Rails.logger.formatter = proc do |severity, timestamp, progname, msg|
+      trace_id = begin
+        span_context = OpenTelemetry::Trace.current_span.context
+        span_context.valid? ? span_context.hex_trace_id : nil
+      rescue
+        nil
+      end
+      
+      formatted = original_formatter.call(severity, timestamp, progname, msg)
+      trace_id ? "[#{trace_id}] #{formatted}" : formatted
+    end
+  end
+  
   Rails.logger.info "Trace ID logging enabled"
 end
